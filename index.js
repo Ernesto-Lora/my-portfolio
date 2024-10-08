269cf8fa5e14691fa4a77341101ed94658dbd598
Ernesto-Lora <98838975+Ernesto-Lora@users.noreply.github.com>
2024-10-08 10:59:44 -0600

    spam

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get("/students",(req, res)=>{
     res.render('students_drop_out.ejs');
 })
 
+app.get("/air-quality",(req, res)=>{
+    res.render('air_quality_calibration.ejs');
+})
+
+app.get("/credit_card",(req, res)=>{
+    res.render('credit_card.ejs');
+})
+
+app.get("/spam_detection",(req, res)=>{
+    res.render('spam.ejs');
+})
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}.`);
