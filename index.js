b62ff3b143a5660f224be53aa3191088eeb20a14
Ernesto-Lora <98838975+Ernesto-Lora@users.noreply.github.com>
2025-05-28 09:12:54 -0600

    server changes

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static("public"));
+// Required if using ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Set EJS as view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// routes
 app.get("/",(req, res)=>{
     res.render('index.ejs');
 })
